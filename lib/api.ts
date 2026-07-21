a1791a406107f6da7fafb9d303f5ba9286534976
mahfujulhoque12 <mdmahfujulhoque123@gmail.com>
2026-07-21 21:44:09 +0600

    add

--- lib/api.ts
+++ lib/api.ts
@@ -7,3 +7,13 @@ export const api = axios.create({
   baseURL: BASE_URL_PRODUCTION,
   withCredentials: true,
 });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
